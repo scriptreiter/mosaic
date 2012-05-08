2c49907efce2e17cb02bf8ce8eb3f042a330cbea
Nicholas Reiter <nreiter@cs.washington.edu>
2012-05-08 00:34:55 -0700

    Structural Changes and DB
    Moved methods to helper files in order to keep the controller 'lean.'
    Also, began populating the db with images, running the flickr scrape
    continuously in a brute force method for now. The page is set with a
    meta refresh every 5 seconds, and scrapes 500 images each time, adding
    any new "colors" to the system.

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -6,55 +6,11 @@ include Magick
 
 class ScrapeController < ApplicationController
   def flickr
-  	
-  	#Flick API REST request information
-  	params = "api_key=d9887d18d9d02626a29194fb14dd8acc&method=flickr.photos.getRecent&format=json&nojsoncallback=1"
-	url = "http://api.flickr.com/services/rest/?#{params}"
-	
-	#Send Flickr REST request and parse JSON
-	response = Net::HTTP.get_response(URI.parse(url)).body
-	response = JSON.parse(response)
+  	request = FlickrRequest.new("flickr.photos.getRecent")
+  	request.send
+  	request.process
 
-	#Loop through the response images
-	@urls = []
-	@colors = []
-
-	if(!response["stat"] == 'ok')
-		puts("ERROR: Flickr Response Not Valid\n")
-	end
-
-	for i in (0...100)
-		img = response["photos"]["photo"][i]
-		img_url = "http://farm#{img['farm']}.staticflickr.com/#{img['server']}/#{img['id']}_#{img['secret']}_s.jpg"
-		color = analyzeImage(img_url)
-
-		@Image = Images.new
-		@Image.color = color
-		@Image.url = img_url
-		@Image.save
-		
-		@urls << img_url
-		@colors << ("#" << color)
-	end
-  end
-  def analyzeImage(file)
-	img = ImageList.new(file)
-	total = [0,0,0]
-	for i in (1..img.columns)
-		row = [0,0,0]	
-		for j in (1..img.rows)
-			px = img.pixel_color(i - 1, j - 1)
-			row[0] += px.red % 256
-			row[1] += px.green % 256
-			row[2] += px.blue % 256
-		end
-		total[0] += (row[0] / img.rows)
-		total[1] += (row[1] / img.rows)
-		total[2] += (row[2] / img.rows)
-	end
-
-	average = [total[0] / img.rows, total[1] / img.rows, total[2] / img.rows]
-	
-	return "" << average[0].to_i.to_s(16).rjust(2,'0') << average[1].to_i.to_s(16).rjust(2,'0') << average[2].to_i.to_s(16).rjust(2,'0')
+  	@colors = request.getAnalyzedColors
+  	@urls = request.getAnalyzedURLs
   end
 end
--- app/models/images.rb
+++ app/models/images.rb
@@ -1,3 +1,4 @@
 class Images < ActiveRecord::Base
   attr_accessible :color, :url
+  validates_uniqueness_of :color, :url
 end
